a3e630d2213621c47cbb204b001a550217158640
Tomasz Borek <tomasz.borek.89@gmail.com>
2023-05-05 23:51:22 +0200

    Created new code html for gallery.

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+    const galleryHtml = `<div class = "gallery_item">
+                            <a class = "gallery_link" href = "${item.original}">
+                                <img
+                                    class = "gallery_image"
+                                    src = "${item.preview}"
+                                    data-source = "${item.original}"
+                                    alt = "${item.description}"
+                                />
+                            </a>
+                        </div>`
+    
+    galleryList.insertAdjacentHTML("afterbegin", galleryHtml);
+
+});
+
