4dcc34faa6ed8da4f4ce1c1209ca1b403e577309
Tomasz Borek <tomasz.borek.89@gmail.com>
2023-05-07 01:25:49 +0200

    Done task 02

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,33 @@
+// import simpleLightbox from '/simplelightbox';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+galleryItems.forEach(event => {
+
+    const galleryHtml = `
+        <li>
+            <a class = "gallery_item" href = "${event.original}">
+                <img 
+                    class = "gallery_image" 
+                    src = "${event.preview}"
+                    alt = "${event.description}">
+            </a>
+        </li>`
+
+    galleryList.insertAdjacentHTML('beforeend', galleryHtml);
+});
+
+const newGallery = new SimpleLightbox('.gallery a' , {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: '250',
+    captionPosition: 'bottom'
+});
+
+
+
+
